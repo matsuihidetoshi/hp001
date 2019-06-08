1030ab825cea2505c4ed7c670fd477908080c939
matsuihidetoshi <partyrockeverydayimshufflin@gmail.com>
2019-06-08 16:56:18 +0900

    buiding user and login functions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  include SessionsHelper
+  
+  private
+  
+  def require_user_logged_in
+    unless logged_in?
+      redirect_to login_url
+    end
+  end
+
+  def correct_user
+    user = User.find(params[:id])
+    unless user == current_user
+      redirect_to root_url
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,25 @@
 Rails.application.routes.draw do
+  get 'users/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/edit'
+
+  get 'users/update'
+
+  get 'users/destroy'
+
   root to: 'statics#index'
+  
   resources :statics
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
